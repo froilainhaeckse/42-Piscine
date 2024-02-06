c76499ecc7641dd2609cc2a3b8273a74df3aee9d
Thea Kupler <tkupler@c4b4c4.42berlin.de>
2024-02-06 13:21:12 +0100

    add main file

---
+++ 2_C00/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkupler <tkupler@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/04 13:03:54 by tkupler           #+#    #+#             */
+/*   Updated: 2024/02/05 15:57:07 by tkupler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_print_alphabet(void);
+void	ft_print_comb(void);
+void	ft_print_comb2(void);
+void	ft_putnbr(int nb);
+void	ft_print_combn(int n);
+
+int	main(void)
+{
+	ft_print_comb2();
+	return (0);
+}
