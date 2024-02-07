11fa0f5025619d078804dcc438a60b9cfebe5ba8
Thea Kupler <tkupler@c4b4c4.42berlin.de>
2024-02-07 18:55:06 +0100

    add implementation to also take of negative values for ex07 for C00

--- 2_C00/ex07/ft_putnbr.c
+++ 2_C00/ex07/ft_putnbr.c
@@ -6,27 +6,42 @@
 /*   By: tkupler <tkupler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 13:51:12 by tkupler           #+#    #+#             */
-/*   Updated: 2024/02/06 13:05:41 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/02/07 13:12:53 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 void	ft_putnbr(int nb);
 
+char	num_to_char(int digit)
+{
+	char	digit_char;
+
+	digit_char = digit + '0';
+	return (digit_char);
+}
+
 void	ft_putnbr(int nb)
 {
-	char	buffer[20];
-	int		digit_count;
-	int		counter;	
-
-	digit_count = snprintf(buffer, sizeof(buffer), "%d", nb);
-	counter = 0;
-	while (counter <= digit_count)
+	char	num_char;
+	int		new_number;
+	int		last_digit;
+	
+	if (nb < 0)
 	{
-		write(1, &buffer[counter], 1);
-		counter++;
+		write(1, "-", 1);
+		nb = -nb;
 	}
-	write(1, "\n", 1);
+	num_char = num_to_char(nb);
+	if (nb < 10)
+	{
+		write(1, &num_char, 1);
+		return ;
+	}
+	new_number = nb / 10;
+	last_digit = nb % 10;
+	ft_putnbr(new_number);
+	num_char = num_to_char(last_digit);
+	write(1, &num_char, 1);
 }
