80bf764961a4a558a1b48660f156c7f7537ce200
froilainhaeckse <thea_kupler@riseup.net>
2024-02-10 21:09:38 +0100

    excluded error handling in seperate file

---
+++ Rush01/ex00/error_handling.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_handling.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 20:56:03 by tkupler           #+#    #+#             */
+/*   Updated: 2024/02/10 21:06:10 by tkupler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void	write_error_message(void)
+{
+	write(1, "Error\n", 6);
+	write(1, "Parse all your digits in one command. ", 39);
+	write(1, "Only use digits between 0 and 4.\n", 34);
+	write(1, "Each digit has to be seperated by one space. ", 46);
+	write(1, "Everything placed within Quotes once.\n", 39);
+	exit(0);
+}
+
+void	error_handling(int argc, char *input)
+{
+	int		i;
+	bool	wrong_argument_count;
+	bool	characters_within_range;
+    bool    error_found;
+
+	i = 0;
+    error_found = false;
+	wrong_argument_count = (argc != 2);
+	if (wrong_argument_count)
+		error_found = true;
+	while (input[i] != '\0')
+	{
+		characters_within_range = (input[i] >= '1' && input[i] <= '4');
+		if (characters_within_range && input[i + 1] == '\0')
+			break ;
+		if (!(characters_within_range && input[i + 1] == 32))
+			error_found = true;
+		i += 2;
+	}
+    if (error_found)
+        write_error_message();
+}
--- Rush01/ex00/main.c
+++ Rush01/ex00/main.c
@@ -6,14 +6,14 @@
 /*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 16:46:49 by tkupler           #+#    #+#             */
-/*   Updated: 2024/02/10 20:51:15 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/02/10 21:08:25 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
+
+void error_handling(int argc, char *input);
 
 void	parse_input(char *input, char *viewpoints)
 {
@@ -22,43 +22,12 @@ void	parse_input(char *input, char *viewpoints)
 	j = 0;
 	while (*input != '\0')
 	{
-		if (*input >= '1' && *input <= '4')
+		if (*input != 32)
 		{
 			viewpoints[j] = *input;
 			j++;
 		}
 		input++;
-	}
-}
-
-void	write_error_message(void)
-{
-	write(1, "Error\n", 6);
-	write(1, "Parse all your digits in one command. ", 39);
-	write(1, "Only use digits between 0 and 4.\n", 34);
-	write(1, "Each digit has to be seperated by one space. ", 46);
-	write(1, "Everything placed within Quotes once.\n", 39);
-	exit(0);
-}
-
-void	error_handling(int argc, char *input)
-{
-	int		i;
-	bool	wrong_argument_count;
-	bool	characters_within_range;
-
-	i = 0;
-	wrong_argument_count = (argc != 2);
-	if (wrong_argument_count)
-		write_error_message();
-	while (input[i] != '\0')
-	{
-		characters_within_range = (input[i] >= '1' && input[i] <= '4');
-		if (characters_within_range && input[i + 1] == '\0')
-			break ;
-		if (!(characters_within_range && input[i + 1] == 32))
-			write_error_message();
-		i += 2;
 	}
 }
 
@@ -69,7 +38,7 @@ int	main(int argc, char **argv)
 
 	input = argv[1];
 	error_handling(argc, input);
-	printf("Back here again!!!\n");
 	parse_input(input, viewpoints);
+	printf("Back here again!!!\n");
 	return (0);
 }
