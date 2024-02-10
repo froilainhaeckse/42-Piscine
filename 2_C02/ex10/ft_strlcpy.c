3fb89ead5260856d93483615a66922bcf2bb0e00
froilainhaeckse <thea_kupler@riseup.net>
2024-02-10 09:25:42 +0100

    more norminette

--- 2_C02/ex10/ft_strlcpy.c
+++ 2_C02/ex10/ft_strlcpy.c
@@ -6,18 +6,18 @@
 /*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 01:02:26 by tkupler           #+#    #+#             */
-/*   Updated: 2024/02/10 01:13:19 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/02/10 09:25:22 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 
 	i = 0;
 	while (src[i] != '\0' && i < size - 1)
 	{
-        dest[i] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
