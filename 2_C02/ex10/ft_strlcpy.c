b0827252d04591223d50bd81f27deb483964bba5
froilainhaeckse <thea_kupler@riseup.net>
2024-02-15 19:33:10 +0100

    add ex10

--- 2_C02/ex10/ft_strlcpy.c
+++ 2_C02/ex10/ft_strlcpy.c
@@ -3,23 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkupler <tkupler@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 01:02:26 by tkupler           #+#    #+#             */
-/*   Updated: 2024/02/13 15:38:31 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/02/15 19:32:42 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
+	unsigned int	length;
 
 	i = 0;
-	while (i < size && src[i] != '\0')
+	if (size != 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
-	return (i);
+	length = ft_strlen(dest);
+	return (length);
 }
