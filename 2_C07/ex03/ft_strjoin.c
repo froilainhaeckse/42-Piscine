51e346082857d7a6a8102f8460c7c6c70826c5d0
froilainhaeckse <thea_kupler@riseup.net>
2024-02-19 00:03:32 +0100

    add more

--- 2_C07/ex03/ft_strjoin.c
+++ 2_C07/ex03/ft_strjoin.c
@@ -3,78 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkupler <tkupler@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 13:41:41 by tkupler           #+#    #+#             */
-/*   Updated: 2024/02/18 19:55:12 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/02/18 23:36:33 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-
-int	ft_strlen(char *str)
-{
-	int	len;
-
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
-}
+#include <stdio.h>
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	int	total_length; //size_t?
+	char	*result;
+	int		i;
+	int		j;
+	int		k;
 
-	int	num_separators;
-	int	i;
-	int	j;
-	int	sep_len;
-	char *result;
-	char *dst;
-	char *str;
-
-	if (size == 0) {
-		char *result = malloc(1);
-		if (result) {
-			result[0] = '\0';
-		}
-		return result;
-	}
-
+	result = malloc(sizeof(strs));
 	i = 0;
-	total_length = 0;
-	while(i < size)
+	k = 0;
+	while (i < size)
 	{
-		total_length += ft_strlen(strs[i]);
-		if (i < size - 1)
-			total_length += ft_strlen(sep);
-		i++;
-	}
-	result = (char *)malloc(total_length * sizeof(char));
-	if (!result)
-		return (0);
-	dst = result;
-	i = 0;
-	while(i < size)
-	{
-		str = strs[i];
-		if (*str)
+		j = 0;
+		while (strs[i][j] != '\0')
 		{
-			while (*str)
-				*dst++ = *str++;
-			if (i < num_separators)
-			{
-				j = 0;
-				while (j < sep_len)
-				{
-					*dst++ = sep[j];
-					j++;
-				}
-			}
+			result[k++] = strs[i][j++];
+		}
+		j = 0;
+		while (sep[j] && i != size - 1)
+		{
+			result[k++] = sep[j++];
 		}
 		i++;
 	}
-	*dst = '\0';
-	return result;
+	result[k] = '\0';
+	return (result);
 }
+
+/*int	main(void)
+{
+	char	*tab[4];
+	tab[0] = "hello";
+	tab[1] = "this";
+	tab[2] = "is";
+	tab[3] = "something";
+	printf("%s", ft_strjoin(4, tab, "seperator"));
+	return (0);
+}*/
