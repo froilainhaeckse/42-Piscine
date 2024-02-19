b7fbfb111480dba1012f6bf1680f8d63b4835e98
Thea Kupler <tkupler@c4b5c3.42berlin.de>
2024-02-19 13:00:32 +0100

    add exercises

--- 2_C05/ex02/ft_iterative_power.c
+++ 2_C05/ex02/ft_iterative_power.c
@@ -3,19 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_iterative_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: tkupler <tkupler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 23:15:35 by tkupler           #+#    #+#             */
-/*   Updated: 2024/02/14 23:23:40 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/02/19 13:00:14 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_iterative_power(int nb, int power)
 {
 	int	i;
 	int	result;
 
-	if (power < 0 || nb == 0)
+	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
@@ -28,3 +30,11 @@ int	ft_iterative_power(int nb, int power)
 	}
 	return (result);
 }
+
+/*int	main(void)
+{
+	printf("%d\n", ft_iterative_power(-4072, 0));
+	printf("%d\n", ft_iterative_power(-1204, 1));
+	printf("%d\n", ft_iterative_power(0, 0));
+	printf("%d\n", ft_iterative_power(5, 2));
+}*/
